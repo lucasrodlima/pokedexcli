94ff282b00ce24dcda1a49b1e9a935c2fb7be511
lucasrodlima <lucasrodlima27@gmail.com>
2025-07-27 12:10:21 -0300

    exit and help commands

--- main.go
+++ main.go
@@ -7,6 +7,43 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cliCommands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+
+`)
+	return nil
+
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
@@ -18,7 +55,17 @@ func main() {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
+
 		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was %v\n", cleanedInput[0])
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		command, ok := cliCommands[cleanedInput[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
 	}
 }
